675df0cc670a63eaed94150c23b85e8ac8b69c10
haytam <haytamraba11@gmail.com>
2026-02-16 20:17:58 +0000

    br2

---
+++ collab/src/App.jsx
@@ -0,0 +1,13 @@
+import Haytam from './components/Haytam.jsx'
+
+function App() {
+  
+
+  return (
+    <>
+      <Haytam />
+    </>
+  )
+}
+
+export default App
---
+++ collab/src/components/Haytam.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Haytam.css'
+
+const Haytam = () => {
+
+   
+
+
+  return (
+    <div>
+
+        <div className='square1'>haytam</div>
+        <div className='square2'>ayman</div>
+        <div className='square3'></div>
+
+    </div>
+    
+  );
+}
+
+export default Haytam
