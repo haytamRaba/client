27c9324677f7c4b0b2b78c75c97f7bf64827b8aa
unknown <yassinejaabouk77@example.com>
2026-02-18 23:18:44 +0000

    NavBar done

---
+++ gym/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import  Navbar  from './components/navbar/Navbar.jsx'
+
+const App = () => {
+  return (
+    <div>
+        <Navbar />
+    </div>
+  )
+}
+export default App
