239f0b1c1b93402245f298b1697bbde90a6cbbb2
haytam <haytamraba11@gmail.com>
2026-02-23 22:17:47 +0000

    explain: usestate

--- gym/src/App.jsx
+++ gym/src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home.jsx";
 import Members from "./pages/members/Members.jsx";
 import ChiffreAffaire from "./pages/chiffreAffaire/ChiffreAffaire.jsx";
+import Payments from "./pages/payments/Payments.jsx";
+import Ex from "./pages/example/Example.jsx";
 const App = () => {
   return (
     <div>
@@ -10,6 +12,8 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/members" element={<Members />} />
         <Route path="/chiffreaffaire" element={<ChiffreAffaire />} />
+        <Route path="/ex" element={<Ex />} />
+        <Route path="/payments" element={<Payments />} />
       </Routes>
       
     </div>
--- gym/src/pages/example/Example.jsx
+++ gym/src/pages/example/Example.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { useState } from 'react'
+import './Example.css'
 
 const Example = () => {
+   const [c, setC] = useState(1);
+
+   const handlerincr = () => {
+        console.log(`iteration :1 ${c}`);
+      setC(c-1);
+        console.log(`iteration :2 ${c}`);
+      setC(c-1);
+        console.log(`iteration :3 ${c}`);
+      setC(c-1);
+   }
+
+   function handlerdecr(){
+    setC(c+1);
+   }
+
+
   return (
-    <div> page Example</div>
+
+    <div className='section'>
+       <button onClick={handlerincr}>-</button>
+        <span>{c}</span>
+       <button onClick={handlerdecr}>+</button>
+    </div>
   )
 }
 
